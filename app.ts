2e7a9f867b3c77ad512c04fbd37ee189d0aeb8ed
dilshod <dilshodx516@gmail.com>
2024-12-22 05:37:02 +0500

    ts

---
+++ app.ts
@@ -0,0 +1,111 @@
+// N1
+// {
+//   function juftFn (n: number): number {
+//     let a = Math.floor(n / 2)
+//     let natija = a * (a + 1)
+//     return natija
+//   }
+
+//   const n: number = 7
+//   console.log(`Juft sonlar yig'indisi: ${juftFn(n)}`)
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+// N2
+// {
+//   function boolFn (b: boolean): number {
+//     if (b === true) {
+//       return 1
+//     } else {
+//       return 0
+//     }
+//   }
+//   const b: boolean = true
+//   console.log(`Natija: ${boolFn(b)}`)
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+// N3
+// {
+//   function nameFn (name: string): string {
+//     if (name) {
+//       return name.slice(0, 2)
+//     } else {
+//       return ''
+//     }
+//   }
+//   const name: string = 'Laylo'
+//   console.log(nameFn(name))
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+//N4
+// {
+//   function integerFn (a: number): number {
+//     return (a = Math.trunc(a + 2))
+//   }
+//   const a: number = 10.3
+//   console.log(`Natija: ${integerFn(a)}`)
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+// N5
+// {
+//   function integerStrFn (b: number): string {
+//     return Math.trunc(b + 1).toString()
+//   }
+//   const b: number = 15.2
+//   console.log(`Natija: ${integerStrFn(b)}`)
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+// N6
+// {
+//   function loopFn (str: string): void {
+//     for (let i = 0; i < str.length; i++) {
+//       console.log(`"${str[i]}"`)
+//     }
+//   }
+//   const str: string = 'Salom'
+//   loopFn(str)
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+// N7
+// {
+//   function reverseFn (num: number): void {
+//     for (let i = num; i >= -num; i--) {
+//       console.log(i)
+//     }
+//   }
+//   const num: number = 2
+//   reverseFn(num)
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+//N8
+// {
+//   function boolFn (num: number): boolean {
+//     return num === 1
+//   }
+//   const num: number = 1
+//   console.log(boolFn(num))
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+//N9
+
+// {
+//   function greeting (name: string, nation: string): void {
+//     console.log(`Hello my name is ${name} I'm ${nation}`)
+//   }
+//   greeting('Laylo', 'Uzbek')
+// }
